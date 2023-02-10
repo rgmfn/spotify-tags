c5791655e2463097207d79c43616e01fc512a88d
ubuntu <rygragg@gmail.com>
2023-02-10 14:43:10 -0800

    Spotify search completed

--- frontend/src/components/SpotifyResults.jsx
+++ frontend/src/components/SpotifyResults.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 
 import {emptySong} from './emptySong.js';
-import {getSong, getSearch} from './httpCalls.js';
+import {getSong, getSearchByURL, getSearch} from './httpCalls.js';
+
+// const emptyResults = {
+//   tracks: {
+//     items: [],
+//   },
+// };
 
 /**
  * @return {object} JSX
  */
 function SpotifyResults({searchQuery, accessToken,
   setAccessToken, refreshToken, refreshTokenFunc, setSongToView}) {
-  const [searchResults, setSearchResults] = React.useState([emptySong]);
+  const [songList, setSongList] = React.useState([emptySong]);
+  // const [searchResults, setSearchResults] = React.useState([emptyResults]);
+  const [nextSongsURL, setNextSongsURL] = React.useState([]);
 
   React.useEffect(() => {
     getSearch(accessToken, refreshToken, setAccessToken,
       refreshTokenFunc, searchQuery).then((results) => {
-      setSearchResults(results.tracks.items);
+      console.log(results);
+      setSongList(results.tracks.items);
+      setNextSongsURL(results.tracks.next);
     });
   }, [searchQuery, accessToken, setAccessToken,
     refreshToken, refreshTokenFunc]);
@@ -22,10 +32,17 @@ function SpotifyResults({searchQuery, accessToken,
     if (event.currentTarget.id) {
       getSong(accessToken, refreshToken, setAccessToken,
         refreshTokenFunc, event.currentTarget.id).then((song) => {
-        console.log(song);
         setSongToView(song);
       });
     }
+  });
+
+  const clickedMoreResults = (() => {
+    getSearchByURL(accessToken, refreshToken, setAccessToken,
+      refreshTokenFunc, nextSongsURL).then((results) => {
+      setSongList(songList.concat(results.tracks.items));
+      setNextSongsURL(results.tracks.next);
+    });
   });
 
   return (
@@ -37,31 +54,38 @@ function SpotifyResults({searchQuery, accessToken,
       </div>
       <table>
         <tbody>
-          {searchResults.map((result) => (
-            <tr
-              id={result.id}
-              onClick={clickedOnSong}
-            >
-              <td className="search-img-col">
-                <div className="imgContainer">
-                  <img
-                    className="search-img"
-                    src={result.album.images[0].url}
-                    alt={'[' + result.album.name + ' img]'}
-                  />
-                </div>
-              </td>
-              <td className="search-name-col">
-                {result.name}
-              </td>
-              <td className="search-artist-col">
-                {result.artists[0].name}
-              </td>
-              <td className="search-album-col">
-                {result.album.name}
-              </td>
-            </tr>
-          ))}
+          {songList.map(
+            (result) => (
+              <tr
+                id={result.id}
+                onClick={clickedOnSong}
+              >
+                <td className="search-img-col">
+                  <div className="imgContainer">
+                    <img
+                      className="search-img"
+                      src={result.album.images[0].url}
+                      alt={'[' + result.album.name + ' img]'}
+                    />
+                  </div>
+                </td>
+                <td className="search-name-col">
+                  {result.name}
+                </td>
+                <td className="search-artist-col">
+                  {result.artists[0].name}
+                </td>
+                <td className="search-album-col">
+                  {result.album.name}
+                </td>
+              </tr>
+            ))}
+          <tr id="more-results" onClick={clickedMoreResults}>
+            <td/>
+            <td/>
+            <td>More results...</td>
+            <td/>
+          </tr>
         </tbody>
       </table>
     </div>
