d431d94db38b190bdefdab7a7394e1c3abc9a9a2
rgmfn <77952163+rgmfn@users.noreply.github.com>
2023-02-10 14:45:13 -0800

    removed searchQuery (show library)
    hand of god comes in to fix the mistakes of mortals

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -81,7 +81,7 @@ function Home() {
   // used to keep track of the current playing status 'isPlaying'
   const [songToView, setSongToView] = React.useState(emptySong);
   // const [searchQuery, setSearchQuery] = React.useState('');
-  const [searchQuery] = React.useState('sobs cool');
+  const [searchQuery] = React.useState('');
 
   React.useEffect(() => {
     const hash = window.location.hash;
