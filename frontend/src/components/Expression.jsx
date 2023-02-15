71784d9ca868b04b4095747aec44bf5c441e19d0
Armaan Deol <armaandeol@Armaans-MacBook-Pro.local>
2023-02-15 14:20:21 -0800

    Remove tags from expression when clicked.

--- frontend/src/components/Expression.jsx
+++ frontend/src/components/Expression.jsx
@@ -8,14 +8,24 @@ import './Expression.css';
  * @return {object} JSX
  */
 function Expression({expression}) {
+  // New state to update the expression as tags are filtered.
+  const [newExpression, setExpression] = React.useState(expression);
+  // Removes tags from the expression when they are clicked.
+  const removeExpression = ((event) => {
+    setExpression(newExpression.filter((tag) =>
+      tag.name !== event.currentTarget.textContent,
+    ));
+  });
   return (
     <div id="expression-container">
       <div id="expression">
-        {expression.length === 0 ? 'Click to build expression...' :
-          expression.map((tag) => (
+        {newExpression.length === 0 ? 'Click to build expression...' :
+          newExpression.map((tag) => (
             <div
               style={{backgroundColor: tag.color}}
               className="tagName"
+              // Remove tag from Expression if clicked.
+              onClick={removeExpression}
             >
               {tag.name}
             </div>
