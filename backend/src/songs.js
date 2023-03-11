b5efc18c2e6fc4e3dc5bf2567ff4e8ff604fa648
ubuntu <rygragg@gmail.com>
2023-03-10 18:16:41 -0800

    removed duplicate function

--- backend/src/songs.js
+++ backend/src/songs.js
@@ -45,16 +45,6 @@ exports.deleteSong = async (req, res) => {
   res.status(200).send();
 };
 
-exports.getAllTags = async (req, res) => {
-  let tagList = null;
-  tagList = await db.allTags(req.query.userid);
-  if (tagList.length === 0) {
-    res.status(404).send();
-  } else {
-    res.status(200).json(tagList);
-  }
-};
-
 exports.postUpdate = async (req, res) => {
   try {
     await db.updateTags(req.body.userid, req.body.spotifyid, req.body.tags);
