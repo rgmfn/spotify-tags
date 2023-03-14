8dff730a147606b18832c987d0ad9bdfdf14192a
Armaan Deol <armaandeol@Armaans-MacBook-Pro.local>
2023-03-14 06:19:09 -0700

    Finished progress bar

--- frontend/src/components/Player.jsx
+++ frontend/src/components/Player.jsx
@@ -7,8 +7,19 @@ import PauseCircleIcon from '@mui/icons-material/PauseCircle';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
 import {ThemeProvider} from '@mui/material/styles';
 import {theme} from './Theme.js';
+import {styled} from '@mui/material/styles';
+
+const TinyText = styled(Typography)({
+  fontSize: '0.75rem',
+  opacity: 0.60,
+  fontWeight: 500,
+  letterSpacing: 0.2,
+});
 
 /**
  * @param {string} accessToken
@@ -18,12 +29,24 @@ import {theme} from './Theme.js';
  * @param {array} updatedLib
  * @return {object} JSX
  */
-function Player({accessToken, clickedTrackID, playingTrackID, 
+function Player({accessToken, clickedTrackID, playingTrackID,
   setPlayingTrackID, updatedLib}) {
   const [player, setPlayer] = React.useState(undefined);
   const [deviceID, setDeviceID] = React.useState(undefined);
-  const [isPaused, setIsPaused] = React.useState(false);
+  const [isPaused, setIsPaused] = React.useState(true);
   const [libraryHasUpdated, setLibraryHasUpdated] = React.useState(false);
+  const [duration, setDuration] = React.useState(0); // seconds
+  const [position, setPosition] = React.useState(0);
+  /**
+   *
+   * @param {*} value
+   * @return {object}
+   */
+  function formatDuration(value) {
+    const minute = Math.floor(value / 60);
+    const secondLeft = value - minute * 60;
+    return `${minute}:${secondLeft < 10 ? `0${secondLeft}` : secondLeft}`;
+  }
   /**
    * Sets up web player to stream music.
    */
@@ -84,7 +107,8 @@ function Player({accessToken, clickedTrackID, playingTrackID,
         if (!state) {
           return;
         }
-
+        setPosition(Math.floor(state.position/1000));
+        setDuration(Math.floor(state.duration/1000));
         // eslint-disable-next-line camelcase
         setPlayingTrackID(state.track_window.current_track.id);
         // eslint-disable-next-line camelcase
@@ -103,6 +127,15 @@ function Player({accessToken, clickedTrackID, playingTrackID,
       });
     };
   }, [accessToken]);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isPaused) {
+        setPosition((prevPosition) => prevPosition + 1);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [isPaused]);
 
   /**
    * If web player is connected to device & song is clicked on,
@@ -140,29 +173,29 @@ function Player({accessToken, clickedTrackID, playingTrackID,
   }, [clickedTrackID]);
 
   React.useEffect(() => {
-  if (player && deviceID && updatedLib.length > 0 && libraryHasUpdated) {
-    console.log(`Player: update playlist`);
+    if (player && deviceID && updatedLib.length > 0 && libraryHasUpdated) {
+      console.log(`Player: update playlist`);
 
-    // creates list of uris (playlist) from list of songs (updatedLib)
-    let playlist = [];
-    updatedLib.forEach((song) => {
-      playlist = [...playlist, song.uri];
-    });
+      // creates list of uris (playlist) from list of songs (updatedLib)
+      let playlist = [];
+      updatedLib.forEach((song) => {
+        playlist = [...playlist, song.uri];
+      });
 
-    // HTTP request to update music
-    fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceID}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        uris: playlist,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${accessToken}`,
-      },
-    });
-    setLibraryHasUpdated(false);
-  }
-}, [playingTrackID]);
+      // HTTP request to update music
+      fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceID}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          uris: playlist,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      });
+      setLibraryHasUpdated(false);
+    }
+  }, [playingTrackID]);
 
   React.useEffect(() => {
     setLibraryHasUpdated(true);
@@ -220,6 +253,41 @@ function Player({accessToken, clickedTrackID, playingTrackID,
             <SkipNextIcon style={{fontSize: 50}} color='secondary'/>
           </IconButton>
         </div>
+        <Box sx={{width: 300}}>
+          <Slider
+            aria-label="song-slider"
+            size="small"
+            value={position}
+            min={0}
+            step={1}
+            max={duration}
+            color='secondary'
+            onChange={(_, value) => fetch(`https://api.spotify.com/v1/me/player/seek?position_ms=${value*1000}`, {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`,
+              },
+            })}
+            sx={{
+              'height': 3,
+              '& .MuiSlider-rail': {
+                opacity: 0.28,
+              },
+            }}
+          />
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              mt: -2,
+            }}
+          >
+            <TinyText>{formatDuration(position)}</TinyText>
+            <TinyText>-{formatDuration(duration - position)}</TinyText>
+          </Box>
+        </Box>
       </ThemeProvider>
     </>
   );
